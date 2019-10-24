c8d0ae0b46280ff9f14f8d016fa9c3179821e52f
Marco Di Meco <marcodimeco@Marcos-MacBook-Pro.local>
2019-10-24 17:07:45 +0200

    refactor: refactor test

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export default class TestComponent extends Component {
     render () {
 
         return (
-            <div>Test Title 6</div>
+            <div>Test Title 7</div>
         )
 
     }
