b4fcc76fc7fb8b357dcfdc3919b0d4e8268ec17a
Marco Di Meco <marcodimeco@Marcos-MacBook-Pro.local>
2019-10-24 16:47:18 +0200

    improvement: improvment description

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export default class TestComponent extends Component {
     render () {
 
         return (
-            <div>Test Title 4</div>
+            <div>Test Title 5</div>
         )
 
     }
